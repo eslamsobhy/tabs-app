ee19418f1f22f47192cf3ce2f7a492849358898a
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-11-01 15:40:00 +0200

    display the first job

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const url = "https://course-api.com/react-tabs-project";
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
 
   const fetchJobs = async () => {
     const response = await fetch(url);
@@ -26,7 +27,33 @@ function App() {
     );
   }
 
-  return <h2>jobs</h2>;
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* btn container */}
+        {/* job info */}
+      </div>
+      <article className="job-info">
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{dates}</p>
+        {duties.map((duty, index) => {
+          return (
+            <div key={index} className="job-desc">
+              <FaAngleDoubleRight className="job-icon" />
+              <p>{duty}</p>
+            </div>
+          );
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
